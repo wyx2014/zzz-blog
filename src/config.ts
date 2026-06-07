1206ed9f797a57c1bb0031a8f6cdf6f8fc3d208c
tree <403145109@qq.com>
2026-06-07 18:54:15 +0800

    feat: integrate Giscus comment module with dynamic theme synchronization

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const GISCUS = {
+  repo: "wyx2014/zzz-blog",
+  repoId: "R_kgDOStjaeA",
+  category: "Announcements",
+  categoryId: "DIC_kwDOStjaeM4C-r40",
+  mapping: "pathname",
+  strict: "0",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "zh-CN",
+  loading: "lazy",
+};
